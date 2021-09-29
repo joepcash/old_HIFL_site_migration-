5a61f9befea49fcb9c358de32cc95c62c0a90954
Joseph Cashell <joepcash@gmail.com>
2021-09-29 09:54:25 +0700

    Added operation to save all webpages

--- src/blog.py
+++ src/blog.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import logging
+import urllib.request, urllib.error, urllib.parse
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +27,16 @@ class Blog:
                 break
 
         return page_urls
+
+    def save_all_pages(self):
+        Path("data/webpages").mkdir(parents=True, exist_ok=True)
+
+        urls = self.get_all_page_urls()
+        for i in range(len(urls)):
+            logger.debug(f"Downloading {urls[i]}")
+            response = urllib.request.urlopen(urls[i])
+            webContent = response.read()
+
+            f = open(f"data/webpages/page{i}.html", 'wb')
+            f.write(webContent)
+            f.close()
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import logging
 import logging.config
 import os
 import json
+import argparse
 
 from blog import Blog
 from page import Page
@@ -20,7 +21,35 @@ def configure_logging(logging_config_path: str) -> None:
     logging.config.dictConfig(logging_config)
     logger.info("Logging configuration set")
 
+
+def get_all_pages(blog_url: str) -> None:
+    logger.info(f"Getting all pages from {blog_url}")
+    blog = Blog(blog_url)
+    blog.save_all_pages()
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', type=str, dest='operation', help="The operation you want to run",
+                        choices=[
+                            "get_all_pages",
+                            "get_all_players",
+                            "get_all_games"
+                        ])
+    parser.add_argument("-b", type=str, default="http://hanoiinternationalfootballleague.blogspot.com/",
+                        dest="blog_url", help="URL of the blog to operate on")
+    args = parser.parse_args()
+
     configure_logging('logging_config.json')
-    blog = Blog("http://hanoiinternationalfootballleague.blogspot.com/")
-    page_urls = blog.get_all_page_urls()
+
+    if args.operation == 'get_all_pages':
+        get_all_pages(args.blog_url)
+
+    # blog = Blog("http://hanoiinternationalfootballleague.blogspot.com/")
+    # pages = [Page(url) for url in blog.get_all_page_urls()]
+    # page1 = Page("http://hanoiinternationalfootballleague.blogspot.com/")
+    # page2 = Page("http://hanoiinternationalfootballleague.blogspot.com/search?updated-max=2016-03-21T17:19:00%2B07:00&max-results=20&start=2&by-date=false")
+    # pages = [Page("http://hanoiinternationalfootballleague.blogspot.com/")]
+
+    # for page in pages:
+    #     page.get_blog_posts()
